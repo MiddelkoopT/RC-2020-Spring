aca6bd3a65d340883d388ea0d063023f4289ea8e
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2020-02-26 15:06:15 -0600

    Simple shell script examples and how to manage the envrionment.

---
+++ examples/hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+print("hello.py> Hello World")
+
---
+++ examples/hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright 2020 by Timothy Middelkoop, License CC by SA 3.0 or APL 2.0
+
+echo === 'hello.sh' $USER $HOME $HOSTNAME $(date)
+
+echo "Hello World"
+
+echo === `python3 --version` `which python3`
+python3 hello.py
+
+echo === $(date)
+
+
+
+
+
