334f0edc3faeb2749e5208f54d227565167fc116
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2020-04-25 14:54:09 -0500

    Add modules.sh use --no-pager

--- benchmark/environment.sh
+++ benchmark/environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+. ./modules.sh
+
 export R_LIBS_USER=./R
-module load r/r-3.6.0-python-3.7.3-tk 
 
 
--- benchmark/modules.sh
+++ benchmark/modules.sh
@@ -1,1 +1,4 @@
-module load openblas/openblas-0.2.20
+#!/bin/bash
+
+module load --no-pager r/r-3.6.0-python-3.7.3-tk
+module load --no-pager openblas/openblas-0.2.20
