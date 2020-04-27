77773f649a289feb4dfbdc82c9b4f11a9f7c84a1
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2020-04-27 15:46:56 -0500

    CPLEX fixed on Clark, removing fix

--- examples/cplex/modules.sh
+++ examples/cplex/modules.sh
@@ -3,5 +3,3 @@
 module load python/python-3.5.2
 module load cplex-studio/cplex-studio-12.7.0
 
-# Fix Python (use 3.5, not 2.7)
-export PYTHONPATH=/cluster/software/cplex-studio/cplex-studio-12.7.0/cplex/python/3.5/x86-64_linux:$PYTHONPATH
